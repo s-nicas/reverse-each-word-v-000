41fc024712d925ffa6858e9c26aab26b54765c5c
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-04-24 19:41:56 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,5 +5,5 @@ def reverse_each_word(string)
   string_to_array.each do |word|
     reversed_words.push(word.reverse)
   end 
-  return reversed_words.join 
+  return reversed_words.join(' ') 
 end 
